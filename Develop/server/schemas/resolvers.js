b955eb150f817b9f41ad502d5423ec1abcf62734
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-09-30 19:36:35 -0500

    unsuccessful debugging

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,106 +1,58 @@
-const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-  Query: {
-    me: async (parent, args, context) => {
-      if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .select('-__v -password')
-          .populate('savedBooks')
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
-        return userData;
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    authenticationError: (parent, args, context) => {
-      throw new AuthenticationError('not authenticated');
-    },
-    apolloError: (parent, args, context) => {
-      throw new ApolloError('not Apollod');
-    },
-    userInputError: (parent, args, context, info) => {
-      if (args.input !== 'expected') {
-        throw new UserInputError('Form Arguments invalid', {
-          invalidArgs: Object.keys(args),
-        });
-      }
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne( { email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { book }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: {savedBooks: book} },
+                    { new: true }
+                )
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                )
+                return updatedUser;
+            }
+        }
     }
-  },
-
-  Mutation: {
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-      return { token, user };
-    },
-    authenticationError: (parent, args, context) => {
-      throw new AuthenticationError('not authenticated');
-    },
-    apolloError: (parent, args, context) => {
-      throw new ApolloError('not Apollod');
-    },
-    userInputError: (parent, args, context, info) => {
-      if (args.input !== 'expected') {
-        throw new UserInputError('Form Arguments invalid', {
-          invalidArgs: Object.keys(args),
-        });
-      }
-    },
-    addUser: async (parent, args) => {
-      console.log('resolvers js')
-      console.log(args);
-      try{
-        const user = await User.create(args);
-      } catch (e) {
-        console.error(e)
-      };
-      const token = signToken(user);
-
-      return { token, user };
-    },
-    saveBook: async (parent, args, context) => {
-      if (context.user) {
-        const book = await Book.create({ ...args, username: context.user.username });
-
-        await User.findByIdAndUpdate(
-          { _id: context.user._id },
-          { $push: { savedBooks: book._id } },
-          { new: true }
-        );
-
-        return User;
-      }
-
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    removeBook: async (parent, { bookId }, context) => {
-      if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: bookId },
-          { $pull: { savedBooks: { bookId: bookId } } },
-          { new: true, runValidators: true }
-        );
-
-        return updatedUser;
-      }
-
-      throw new AuthenticationError('You need to be logged in!');
-    }
-  }
-};
-
+  };
 module.exports = resolvers;
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,56 +1,47 @@
 const { gql } = require('apollo-server-express');
 
+// typeDefs
 const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    email: String
-    bookCount: Int
-    savedBooks: [Book]
-  }
-
-  type Book {
-    _id: ID
-    bookId: String 
-    authors: [String]
-    description: String
-    title: String
-    image: String
-    link: String
-  }
-
-  input bookInput {
-    bookId: String 
-    authors: [String]
-    description: String
-    title: String
-    image: String
-    link: String
-  }
-
-  type Auth {
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        forSale: String
+        link: String
+        title: String
+    }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]        
+    }
+    type Query {
+        me: User
+    }
+    type Auth {
     token: ID!
     user: User
-  }
-
-  type Query {
-    me: User
-    users: [User]
-    user(username: String!): User
-    authenticationError: String  
-    apolloError: String 
-    userInputError: String
-  }
-
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input: bookInput!): User
-    removeBook(bookId: String!): User
-    authenticationError: String  
-    apolloError: String 
-    userInputError: String
-  }
+    }
+    input SavedBookInput {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        forSale: String
+        link: String
+        title: String
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: SavedBookInput): User
+        removeBook(bookId: String!): User
+}
 `;
 
+// export the typeDefs
 module.exports = typeDefs;
